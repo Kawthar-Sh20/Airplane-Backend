d10ab50cd1ccd83bb852e7020561e3c1300fa1dd
hady-awayda <hady.awayda@gmail.com>
2024-07-16 00:56:33 +0300

    Updated

--- router.php
+++ router.php
@@ -1,7 +1,7 @@
 <?php
-header("Access-Control-Allow-Origin: *"); // Allow access from any origin
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS"); // Allow these request methods
-header("Access-Control-Allow-Headers: Content-Type, Authorization"); // Allow these headers
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     // Handle preflight request
