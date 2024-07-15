cb9b4af02c48ebf2a6c5932d31e31e9c3741c015
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-07-15 16:55:50 +0300

    Chneged login and register files

--- src/controllers/login.php
+++ src/controllers/login.php
@@ -1,13 +1,13 @@
-
 <?php
-
 require '../connection.php';
 
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
     $email = $_POST['email'];
     $password = $_POST['password'];
 
-    $stmt = $conn->prepare('SELECT id, email, password, name FROM users WHERE email = ?');
+    $stmt = $conn->prepare('select id,email,password,name
+    from users 
+    where email=?');
     $stmt->bind_param('s', $email);
     $stmt->execute();
     $stmt->store_result();
@@ -16,10 +16,11 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
     $user_exists = $stmt->num_rows;
 
     if ($user_exists == 0) {
-        $res['message'] = "User not found";
+        $res['message'] = "user not found";
     } else {
+
         if (password_verify($password, $hashed_password)) {
-            $res['status'] = "authenticated";
+            $res['status'] = 'authenticated';
             $res['id'] = $id;
             $res['name'] = $name;
             $res['email'] = $email;
@@ -28,10 +29,6 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
         }
     }
 } else {
-    $res = ["error" => "Wrong request method"];
+    echo json_encode(["error" => "Wrong request method"]);
 }
-
 echo json_encode($res);
-
-
-
--- src/controllers/regiser.php
+++ src/controllers/regiser.php
@@ -3,37 +3,29 @@
 require '../connection.php';
 
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
+
+
+    $username = $_POST['username'];
     $email = $_POST['email'];
     $password = $_POST['password'];
+    $name = $_POST['name'];
+    $check_email = $conn->prepare('select id from users where username=? or email=?;');
+    $check_email->bind_param('ss', $username, $email);
+    $check_email->execute();
+    $result = $check_email->get_result();
 
-    $stmt = $conn->prepare('SELECT id, email, password, name FROM users WHERE email = ?');
-    $stmt->bind_param('s', $email);
-    $stmt->execute();
-    $stmt->store_result();
-    $stmt->bind_result($id, $email, $hashed_password, $name);
-    $stmt->fetch();
-    $user_exists = $stmt->num_rows;
-
-    if ($user_exists == 0) {
-        $res['message'] = "user not found";
+    if ($result->num_rows > 0) {
+        echo json_encode(["error" => "Already exists"]);
     } else {
-        echo "unhashed password: $password<br>";
-        echo "hashed password: $hashed_password<br>";
-
-        if (password_verify($password, $hashed_password)) {
-            $res['status'] = "authenticated";
-            $res['id'] = $id;
-            $res['name'] = $name;
-            $res['email'] = $email;
-        } else {
-            $res['status'] = "wrong password";
-        }
+        $hashed_password = password_hash($password, PASSWORD_BCRYPT);
+        $stmt = $conn->prepare('insert into users(name,username,email,password) values (?,?,?,?); ');
+        $stmt->bind_param('ssss', $name, $username, $email, $hashed_password);
+        $stmt->execute();
+        $res['status'] = "success";
+        $res['message'] = "inserted successfully";
+        echo json_encode($res);
+        //echo json_encode(["status"=>"success","message"=>"inserted successfully"]);
     }
 } else {
     echo json_encode(["error" => "Wrong request method"]);
 }
-
-echo json_encode($res);
-
-?>
-
