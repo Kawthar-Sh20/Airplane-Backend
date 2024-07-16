6fc3339b71c5b453267564b5e0647fd29d59becd
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-07-16 19:08:20 +0300

    Changed some files

--- src/controllers/openAIPost.php
+++ src/controllers/openAIPost.php
@@ -1,23 +1,7 @@
 <?php 
 require "../../connection.php";
 
-if ($_SERVER['REQUEST_METHOD'] == "GET") {
-    $id = $_GET['id'];
-    $stmt = $conn->prepare('SELECT c.name, f.id_flight_booking 
-                            FROM flight_bookings f 
-                            JOIN cities c 
-                            ON f.arrival_city_id = c.id_city
-                            WHERE f.id_flight_booking = ?;');
-    $stmt->bind_param('i', $id);
-    $stmt->execute();
-    $result = $stmt->get_result();
-    if ($result->num_rows > 0) {
-        $city = $result->fetch_assoc();
-        echo json_encode(["city" => $city]);
-    } else {
-        echo json_encode(["message" => "no records were found"]);
-    }
-} elseif ($_SERVER['REQUEST_METHOD'] == "POST") {
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
     $user_id = $_POST['user_id'];
     $chat_user_id = $_POST['chat_user_id'];
     $message = $_POST['message'];
@@ -36,4 +20,3 @@ if ($_SERVER['REQUEST_METHOD'] == "GET") {
 } else {
     echo json_encode(["error" => "Wrong request method"]);
 }
-?>
